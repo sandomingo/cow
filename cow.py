350e6cd039d5a9868d342d950d64d5550ce73028
SanDomingo <whyzxm@gmail.com>
2015-03-08 16:36:22 +0800

    init commit

--- cow.py
+++ cow.py
@@ -98,12 +98,9 @@ def read_file_as_string(in_file):
 
 def convert(in_file, out_encoding):
     confidence, guess_encoding = detect(in_file)
-    if confidence > 40.0:
-        read_then_write(in_file, guess_encoding, out_encoding)
-        print 'Convert from %s to %s Completed (with a confidence of %.2f%%)!' \
+    read_then_write(in_file, guess_encoding, out_encoding)
+    print 'Convert from %s to %s Completed (with a confidence of %.2f%%)!' \
               % (guess_encoding, out_encoding, confidence)
-    else:
-        print 'Convert failed! [Debug info] confidence->%s, guess_encoding->%s' % (confidence, guess_encoding)
 
 
 def detect(in_file):
